bc4e0db198d1bd2aa8864908160af3af18340eb6
TatyanaLukashevich <tatyana.lukashevich96@gmail.com>
2016-07-21 13:58:06 +0300

    added day6 && custom section in UserStorage

---
+++ Day6_LINQ/GetHashCode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHashCode
+{
+    class Program
+    {
+        public delegate void Del1(int a);
+        public delegate void Del2(int a);
+        static void Main(string[] args)
+        {
+            //string a = "15";
+            //string b = "15";
+            //var a = new KeyValuePair<int, string>(1, "5");
+            //var b = new KeyValuePair<int, string>(2, "5");
+            //Console.WriteLine("a:{0}. b:{1}", a.GetHashCode(), b.GetHashCode());
+            string a = "heh";
+            StringBuilder b =  new StringBuilder("heh");
+
+           
+        }
+    }
+}
