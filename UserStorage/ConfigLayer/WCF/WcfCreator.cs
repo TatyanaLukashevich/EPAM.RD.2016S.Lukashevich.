0aec8bcdc35f50dc1ec007ce2911b5719eadaa26
TatyanaLukashevich <tatyana.lukashevich96@gmail.com>
2016-08-05 11:01:08 +0300

    version1 and varsion2 added

---
+++ UserStorage/ConfigLayer/WCF/WcfCreator.cs
@@ -0,0 +1,22 @@
+﻿using Replication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigLayer.WCF
+{
+    public class WcfCreator
+    {
+        public static ServiceHost CreateWcf(Service service)
+        {
+           
+            ServiceHost host = new ServiceHost(service);
+            return host;
+        }
+
+
+    }
+}
