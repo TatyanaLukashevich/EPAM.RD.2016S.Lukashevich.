d5619ca9b5669f2d526713a387bc31c248b6b7f6
TatyanaLukashevich <tatyana.lukashevich96@gmail.com>
2016-08-15 01:41:21 +0300

    wcf changes

--- UserStorage/WcfClient/Program.cs
+++ UserStorage/WcfClient/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using Replication;
 using UserStorage;
-using UserStorage.Replication;
 using WcfClient.ServiceReference1;
 
 namespace WcfClient
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
             var user = new User("Amy", "Levis", new DateTime(1996, 2, 17), 0);
 
             USContractClient client = new USContractClient();
@@ -17,8 +17,8 @@ namespace WcfClient
             client.Open();
 
             client.Add(user);
-
             client.Close();
+           
         }
 
     }
